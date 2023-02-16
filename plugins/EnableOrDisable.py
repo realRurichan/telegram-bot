489d23f775e7d4d8c776b659c7d239c73c4e3ed9
realRurichan <rurichan@disroot.org>
2023-02-17 01:07:53 +0800

    fix: permission bug

--- plugins/EnableOrDisable.py
+++ plugins/EnableOrDisable.py
@@ -11,9 +11,9 @@ def load():
     logger.info("EnableOrDisable is loaded.")
 
 async def enable(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_chat.type == "group" or "supergroup":
+    if update.effective_chat.type == "group" or update.effective_chat.type == "supergroup":
         user = await update.effective_chat.get_member(update.effective_user.id)
-        if user.status == "administrator" or "creator":
+        if user.status == "administrator" or user.status == "creator":
             table_name = str(update.effective_chat.id)[4: ]
             res = cur.execute("select count(*)  from sqlite_master where type='table' and name =" + "\"" + table_name + "\"" + ";")
             if int(res.fetchone()[0]) == 0:
@@ -43,15 +43,15 @@ async def enable(update: Update, context: ContextTypes.DEFAULT_TYPE):
             except (IndexError):
                 await update.message.reply_text("用法：\n请在指令后面加上想要禁用的功能\n如（括号内为解释）：\nwelcome（进群欢迎）")
         else:
-            update.message.reply_text("只有群组管理员可以使用此功能哦")
+            await update.message.reply_text("只有群组管理员可以使用此功能哦")
     else:
         await update.message.reply_text("请在群组中使用本功能qwq")
         
 
 async def disable(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_chat.type == "group" or "supergroup":
+    if update.effective_chat.type == "group" or update.effective_chat.type == "supergroup":
         user = await update.effective_chat.get_member(update.effective_user.id)
-        if user.status == "administrator" or "creator":
+        if user.status == "administrator" or user.status == "creator":
             table_name = str(update.effective_chat.id)[4: ]
             res = cur.execute("select count(*)  from sqlite_master where type='table' and name =" + "\"" + table_name + "\"" + ";")
             if int(res.fetchone()[0]) == 0:
@@ -77,7 +77,7 @@ async def disable(update: Update, context: ContextTypes.DEFAULT_TYPE):
             except (IndexError):
                 await update.message.reply_text("用法：\n请在指令后面加上想要禁用的功能\n如（括号内为解释）：\nwelcome（进群欢迎），cancelpin（取消频道自动置顶）")
         else:
-            update.message.reply_text("只有群组管理员可以使用此功能哦")
+            await update.message.reply_text("只有群组管理员可以使用此功能哦")
     else:
         await update.message.reply_text("请在群组中使用本功能qwq")
 
