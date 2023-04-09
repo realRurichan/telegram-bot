8c9b9c9013f2c76389cb2ae17177a835c13cceff
realRurichan <rurichan@disroot.org>
2023-04-10 02:52:58 +0800

    add: poi search for weather plugin

--- plugins/Weather.py
+++ plugins/Weather.py
@@ -22,19 +22,26 @@ async def weather(update: Update, context: ContextTypes.DEFAULT_TYPE):
     geoapi = requests.get(f'https://geoapi.qweather.com/v2/city/lookup?key={qweather_key}&location={target_city}&lang=zh')
     geoapi_result = geoapi.json()
     if (geoapi_result['code'] != '200'):
-        await update.message.reply_text("小兔子查询不到你想要查询的城市哦，请检查后再试试qwq")
-        return
-    location = geoapi_result["location"][0]
-    location_id = location['id']
-    weather_api = requests.get(f'https://devapi.qweather.com/v7/weather/now?key={qweather_key}&location={location_id}&lang=zh')
+        poiapi = requests.get(f'https://geoapi.qweather.com/v2/poi/lookup?key={qweather_key}&type=scenic&location={target_city}&lang=zh')
+        poiapi_result = poiapi.json()
+        if (poiapi_result['code'] != '200'):
+            await update.message.reply_text("小兔子查询不到你想要查询的城市哦，请检查后再试试qwq")
+            return
+    try:
+        location = geoapi_result["location"][0]
+    except:
+        location = poiapi_result["poi"][0]
+    lon = location['lon']
+    lat = location['lat']
+    weather_api = requests.get(f'https://devapi.qweather.com/v7/weather/now?key={qweather_key}&location={lon},{lat}&lang=zh')
     weather_api_result = weather_api.json()
     weather_info = weather_api_result["now"]
-    if location["name"] != location["adm2"]:
+    if (location["name"] != location["adm2"]) and geoapi_result['code'] == '200':
         city_name = location["adm2"] + location["name"]
     else:
         city_name = location["name"]
     try:
-        air_quality_api = requests.get(f'https://devapi.qweather.com/v7/air/now?key={qweather_key}&location={location_id}&lang=zh')
+        air_quality_api = requests.get(f'https://devapi.qweather.com/v7/air/now?key={qweather_key}&location={lon},{lat}&lang=zh')
         air_quality_api_result = air_quality_api.json()
         air_quality_info = air_quality_api_result["now"]
         await update.message.reply_text(f"{city_name} 的天气为：\n"
