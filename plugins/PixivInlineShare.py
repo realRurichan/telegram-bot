ec0bfa83b0986767922c76361057bd4e42783610
realRurichan <rurichan@disroot.org>
2023-04-18 18:05:48 +0800

    add: pixiv inline share

---
+++ plugins/PixivInlineShare.py
@@ -0,0 +1,62 @@
+import os
+from dotenv import load_dotenv
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.constants import ParseMode
+from telegram.ext import ContextTypes, InlineQueryHandler
+from loguru import logger
+from pixivpy_async import PixivClient, AppPixivAPI
+import re
+
+load_dotenv()
+
+PIXIV_URL_REGEX = r'https?://(?:www\.)?pixiv\.net/(?:\w+/)?artworks/(\d+)'
+
+def load():
+    logger.info("Pixiv Inline Share is loaded.")
+
+async def pixiv_inline_share(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    match = re.search(PIXIV_URL_REGEX, query)
+
+    if match:
+        illustration_id = int(match.group(1))
+        print(illustration_id)
+        pixiv_refreshtoken = os.getenv("PIXIV_REFRESHTOKEN")
+        async with PixivClient() as client:
+            api = AppPixivAPI(client=client)
+            await api.login(refresh_token=pixiv_refreshtoken)
+            try:
+                illustration = await api.illust_detail(illustration_id)
+                image_url = illustration.illust.image_urls.large
+            except(AttributeError):
+                await context.bot.answer_inline_query(update.inline_query.id, [])
+                return
+
+            # Thanks for pixiv.cat
+            proxy_image_url = image_url.replace("https://i.pximg.net", "https://i.pixiv.cat")
+
+            results = [
+            InlineQueryResultArticle(
+                id=illustration_id,
+                title=illustration.illust.title,
+                description=f"By {illustration.illust.user.name}",
+                input_message_content=InputTextMessageContent(
+                    message_text=(
+                                f"Pixiv Share\n"
+                                f"[{illustration.illust.title}]({proxy_image_url}) by {illustration.illust.user.name}\n"
+                                f"[Original link]({query})"
+                    ),
+                    parse_mode=ParseMode.MARKDOWN
+                ),
+                thumb_url=proxy_image_url
+            )
+            ]
+            
+            logger.debug(results)
+            await context.bot.answer_inline_query(update.inline_query.id, results)
+        
+
+    else:
+        await context.bot.answer_inline_query(update.inline_query.id, [])
+
+handlers = [InlineQueryHandler(pixiv_inline_share, pattern=PIXIV_URL_REGEX)]
