424c0b67b727bf492aa8e19257c49835b6028a5f
realRurichan <rurichan@disroot.org>
2023-04-19 04:39:59 +0800

    add: version code

---
+++ version.py
@@ -0,0 +1,7 @@
+version = "0.1"
+code = "Aries"
+# The status can have three values:
+# "git" indicates that it is the latest code pulled from the git repository.
+# "stable" indicates that it is a stable version.
+# "testing" indicates that it is a testing version.
+status = "git"
